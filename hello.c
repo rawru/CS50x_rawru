ad0b621b06740f5ca514b0c4008b480366ea4179
rawru <109854605+rawru@users.noreply.github.com>
2022-11-11 22:34:47 +0800

    Delete hello.c

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    // Prompts user for name and prints it.
-    string name = get_string("What is your name?\n");
-    printf("Hello, %s!\n", name);
-}
